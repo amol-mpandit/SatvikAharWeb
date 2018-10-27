45c141987950ee141078d134b8200e47435571ea
Amol Pandit <amol.mpandit@gmail.com>
2018-10-27 18:41:17 +0530

    Add project files.

---
+++ SatvikAhar/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace SatvikAhar.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ SatvikAhar/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SatvikAhar.WebApiApplication" Language="C#" %>
