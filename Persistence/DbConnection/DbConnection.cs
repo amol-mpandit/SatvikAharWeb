5d671bc66cc5e6ed5b90d0e561d232bb1670f7ea
Amol Pandit <amol.mpandit@gmail.com>
2018-11-14 00:25:23 +0530

    added Core and Persistence project

---
+++ Persistence/DbConnection/DbConnection.cs
@@ -0,0 +1,23 @@
+﻿using System.Data;
+
+namespace Persistence.DbConnection
+{
+    public class DbConnection 
+    {
+        public IDbConnection Connection { get; set; }
+
+        public DbConnection(IDbConnection connection)
+        {
+            Connection = connection;
+            EnsureDbConnectionIsOpen();
+        }
+
+        private void EnsureDbConnectionIsOpen()
+        {
+            if (Connection.State == ConnectionState.Closed)
+            {
+                Connection.Open();
+            }
+        }
+    }
+}
