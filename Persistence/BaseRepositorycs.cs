3272a125802b33bcaf0ff36f2808ffd0f890001f
Amol Pandit <amol.mpandit@gmail.com>
2018-11-14 15:08:28 +0530

    added base repository file

---
+++ Persistence/BaseRepositorycs.cs
@@ -0,0 +1,12 @@
+﻿namespace Persistence
+{
+    public abstract class BaseRepository
+    {
+        public DbConnection Connection;
+
+        public BaseRepository(DbConnection dbConnection)
+        {
+            Connection = dbConnection;
+        }
+    }
+}
--- Persistence/DbConnection.cs
+++ Persistence/DbConnection.cs
@@ -1,6 +1,6 @@
 ﻿using System.Data;
 
-namespace Persistence.DbConnection
+namespace Persistence
 {
     public class DbConnection 
     {
