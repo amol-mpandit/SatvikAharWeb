00e8227431e4f5dda2f391d6bd6ebad6bd4e3fab
Amol <apandit@cogitate.us>
2019-02-06 20:22:30 +0530

    -WebApp / webApi prototype -UI Design CSS stylesheet selection

--- Core/Post.cs
+++ Core/Post.cs
@@ -5,5 +5,6 @@
         public string Title { get; set; }
         public string ShortSummery { get; set; }
         public string Summery { get; set; }
+        public string HashTag { get; set; }
     }
 }
---
+++ Core/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core
+{
+    public class User
+    {
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+}
